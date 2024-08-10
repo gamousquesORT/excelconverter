dff4b1a69811a4dde9543a9593d7a09121ec4ba3
Gastón Mousqués <mousques@ort.edu.uy>
2024-08-10 15:48:24 -0300

    extracted functions to simplify code

--- Leerplanillacupos/convert_daily_enrollment_report.py
+++ Leerplanillacupos/convert_daily_enrollment_report.py
@@ -42,6 +42,59 @@ def is_string(cell):
 inline_text_date_regex = re.compile(inline_text_date_pattern)
 file_date_regex = re.compile(file_date_pattern)
 
+
+def select_split_char_named():
+    global split_char , split_column
+    if 'Ins/Cupo' in df.columns:
+        split_char = valid_columns_to_split['Ins/Cupo']
+        split_column = 'Ins/Cupo'
+    elif 'Ins_Cupo' in df.columns:
+        split_char = valid_columns_to_split['Ins_Cupo']
+        split_column = 'Ins_Cupo'
+
+
+def insert_course_session_named():
+    global inscriptos , cupos , row_index
+    inscriptos , cupos = row[split_column].split(split_char)
+    # Add Inscriptos and Cupos columns to the row
+    row['Inscriptos'] = inscriptos.strip()  # remove leading/trailing spaces
+    row['Cupos'] = cupos.strip()
+    row['Fecha'] = date_to_insert.strip()  # remove leading/trailing spaces
+    # Append the row to the filtered_rows list
+    filtered_rows.append(row)
+
+
+def insert_course_session_unnamed():
+    global inscriptos , cupos
+    inscriptos , cupos = df.iloc[row_index , 6].split(split_char)
+    # Add Inscriptos and Cupos columns to the row
+    row['Inscriptos'] = inscriptos.strip()  # remove leading/trailing spaces
+    row['Cupos'] = cupos.strip()
+    row['Fecha'] = date_to_insert.strip()  # remove leading/trailing spaces
+    # Append the row to the filtered_rows list
+    filtered_rows.append(row)
+
+
+
+def set_date_to_insert_named():
+    global date_match , date_to_insert
+    # define date based on file name
+    date_match = file_date_regex.search(file)
+    if date_match:
+        date_to_insert = date_match.group()
+
+
+def select_split_char_unnamed():
+    global value , split_char , split_column
+    for value in row.values:
+        if value in valid_columns_to_split.keys():
+            split_char = valid_columns_to_split[value]
+            split_column = str(value)
+            break
+
+
+
+
 # Iterate over each file in the directory
 for file in files:
     split_char = None
@@ -90,56 +143,36 @@ for file in files:
                 materia_marker_found = True
                 named_column_mode = True
 
-                #define date based on file name
-                date_match = file_date_regex.search(file)
-                if date_match:
-                    date_to_insert = date_match.group()
+                set_date_to_insert_named()
 
-                if 'Ins/Cupo' in df.columns:
-                    split_char = valid_columns_to_split['Ins/Cupo']
-                    split_column = 'Ins/Cupo'
-                elif 'Ins_Cupo' in df.columns:
-                    split_char = valid_columns_to_split['Ins_Cupo']
-                    split_column = 'Ins_Cupo'
+                select_split_char_named()
 
                 nombre_materia = row['Materia']
                 if nombre_materia.strip() in materia_list:
                     try:
-                        inscriptos , cupos = row[split_column].split(split_char)
-                        # Add Inscriptos and Cupos columns to the row
-                        row['Inscriptos'] = inscriptos.strip()  # remove leading/trailing spaces
-                        row['Cupos'] = cupos.strip()
-                        row['Fecha'] = date_to_insert.strip()  # remove leading/trailing spaces
-                        # Append the row to the filtered_rows list
-                        filtered_rows.append(row)
+                        insert_course_session_named()
+                        row_index = row_index + 1
                     except ValueError:
                         # If splitting fails, print a message and continue iterating
                         print("Cannot split Ins_Cupos value in row:" , index)
                         break
-                    row_index = row_index + 1
+
+
             if 'Materia' in row.values and not materia_marker_found:
                 materia_marker_found = True
                 named_column_mode = False
 
-                for value in row.values:
-                    if value in valid_columns_to_split.keys():
-                        split_char = valid_columns_to_split[value]
-                        split_column = str(value)
-                        break
+                select_split_char_unnamed()
                 row_index = row_index + 1
                 continue
+
+
             if named_column_mode is False and materia_marker_found:
                     if is_string(df.iloc[row_index, 3]):
                         nombre_materia = df.iloc[row_index , 3]
                         if nombre_materia.strip() in materia_list:
                             try:
-                                inscriptos , cupos = df.iloc[row_index, 6].split(split_char)
-                                # Add Inscriptos and Cupos columns to the row
-                                row['Inscriptos'] = inscriptos.strip()  # remove leading/trailing spaces
-                                row['Cupos'] = cupos.strip()
-                                row['Fecha'] = date_to_insert.strip()  # remove leading/trailing spaces
-                                # Append the row to the filtered_rows list
-                                filtered_rows.append(row)
+                                insert_course_session_unnamed()
                             except ValueError:
                                 # If splitting fails, print a message and continue iterating
                                 print("Cannot split Ins_Cupos value in row:" , index)
@@ -154,13 +187,7 @@ for file in files:
                 nombre_materia = row['Materia']
                 if nombre_materia.strip() in materia_list:
                     try:
-                        inscriptos , cupos = row[split_column].split(split_char)
-                        # Add Inscriptos and Cupos columns to the row
-                        row['Inscriptos'] = inscriptos.strip()  # remove leading/trailing spaces
-                        row['Cupos'] = cupos.strip()
-                        row['Fecha'] = date_to_insert.strip()  # remove leading/trailing spaces
-                        # Append the row to the filtered_rows list
-                        filtered_rows.append(row)
+                        insert_course_session_named()
                     except ValueError:
                         # If splitting fails, print a message and continue iterating
                         print("Cannot split Ins_Cupos value in row:" , index)
